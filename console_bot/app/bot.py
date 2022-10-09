22eb939c679e6cd710b6c18d083cc9da1b35f07b
ortursucceeh <109922331+ortursucceeh@users.noreply.github.com>
2022-10-09 19:44:46 +0300

    Update bot.py

--- console_bot/app/bot.py
+++ console_bot/app/bot.py
@@ -1,35 +1,42 @@
+import functools
 from collections import UserDict
-
-
-EXIT_WORDS = ["exit", "close", "good bye", "end"]
 
 
 class AdressBook(UserDict):
 
     def add_record(self, record):
-        self.data[record.name.value] = record.phone
-
-
-contacts = AdressBook()
+        self.data[record.name.value] = record
 
 
 class Record:
 
-    def __init__(self, rec_name, *rec_phone):
-        self.name = rec_name
-        self.phone = list(rec_phone)
-
-    def add_phone(self, rec_name, rec_phone):
-        contacts[rec_name.name] = rec_phone.phone
-
-    def remove_phone(self, rec_name, rec_phone):
-        if rec_phone.phone in contacts[rec_name.name]:
-            contacts[rec_name.name].remove(rec_phone.phone)
+    def __init__(self, name=None, phone=None):
+        self.name = Name(name)
+        if phone:
+            self.phones = [Phone(phone)]
         else:
-            raise KeyError
+            self.phones = []
 
-    def edit_record(self, old_rec_name, new_rec_phone):
-        contacts[old_rec_name.name] = new_rec_phone.phone
+    def add_phone(self, record, new_phone):
+        record.phones = [record.phones] + [new_phone]
+        return f"Phone was added to record"
+
+    def remove_phone(self, name, removed_phone):
+        # list of str phones from record.phones
+        names_phones = [i.value for i in contacts[name].phones]
+
+        if removed_phone in names_phones:
+            for phone in contacts[name].phones:
+                if removed_phone == phone.value:
+                    contacts[name].phones.remove(phone)
+                    return f"Phone was removed from record"
+        else:
+            raise KeyError("Removed phone isn't in phones")
+
+    def edit_record(self, record, new_record):
+        record.name.value = new_record.name.value
+        record.phones = new_record.phones
+        return f"Record was changed"
 
 
 class Field:
@@ -38,17 +45,23 @@ class Field:
 
 class Name(Field):
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, value):
+        self.value = value
 
 
 class Phone(Field):
 
-    def __init__(self, phone=None):
-        self.phone = phone
+    def __init__(self, value=None):
+        self.value = value
+
+
+EXIT_WORDS = ["exit", "close", "good bye", "end"]
+
+contacts = AdressBook()
 
 
 def input_error(func):
+    @functools.wraps(func)
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
@@ -66,52 +79,67 @@ def input_error(func):
 
 
 @input_error
-def add_contact(name, number=None):
+def add_contact(record=None):
     """Func which adds new contact in contacts"""
-    if number is None:
+    if record.phones is None:
         raise IndexError
-    elif name in contacts:
-        raise ValueError(f"{name} is already in contacts")
-    contacts[name] = number
-    return f"Contact with name {name} and phone {number} was added."
+    elif record.name.value in contacts:
+        raise ValueError(f"{record.name.value} is already in contacts")
+    contacts.add_record(record)
+    return f"Contact with name {record.name.value} and phones {[i.value for i in record.phones]} was added."
 
 
 @input_error
-def find_contact(name, number=None):
+def find_contact(record=None):
     """Func which shows name's number"""
-    return contacts[name]
+    return contacts[record.name.value]
 
 
 @input_error
-def change_contact(name, new_number=None):
+def change_contact(record=None, new_number=None):
     """Func which changes some contact"""
-    if name in contacts:
-        contacts[name] = new_number
-        return f"Contact's number with name {name} was changed to {new_number}."
+    if record.name.value in contacts:
+        contacts[record.name.value] = new_number
+        return f"Contact's number with name {record.name.value} was changed to {new_number}."
     else:
         raise ValueError("Unknown name.")
 
 
 @input_error
-def show_all_contacts(*args):
+def show_all_contacts(*args, **kwargs):
     """Func which shows all contacts"""
     if contacts:
-        return '\n'.join([f"{key} ---:  {value}" for key, value in contacts.items()])
+        return '\n'.join([f"{key} ---:  {[i.value for i in value.phones]}" for key, value in contacts.items()])
     else:
         return "You don't have any contacts."
 
 
 @input_error
-def remove_contact(name, number=None):
+def remove_contact(record=None):
     """Func which deletes contact"""
-    del contacts[name]
-    return f"Contact with name {name} was removed."
+    del contacts[record.name.value]
+    return f"Contact with name {record.name.value} was removed."
 
 
 @input_error
-def show_all_commands(name=None, number=None):
+def show_all_commands(*args, **kwargs):
     """Func which shows all commands"""
     return " # ".join(handler.keys())
+
+
+@input_error
+def add_record_phone(record=None, new_phone=None):
+    record.add_phone(record, new_phone)
+
+
+@input_error
+def remove_record_phone(record, removed_phone):
+    record.remove_phone(record, removed_phone)
+
+
+@input_error
+def edit_record(record, new_record):
+    record.edit_record(record, new_record)
 
 
 handler = {
@@ -121,21 +149,27 @@ handler = {
     "phone": find_contact,
     "show": show_all_contacts,
     "remove": remove_contact,
-    "commands": show_all_commands
+    "commands": show_all_commands,
+    "add_record_phone": add_record_phone,
+    "remove_record_phone": remove_record_phone,
+    "edit_record": edit_record
 }
 
 
 def main():
     while True:
 
-        command = input("Enter command: ").lower().strip().split()
+        in_command = input(
+            "Enter command and record: ").lower().strip().split()
+        command = in_command[0]
+        record = Record(*in_command[1:])
 
-        if command[0] in EXIT_WORDS:
+        if command in EXIT_WORDS:
             print("See ya!")
             exit()
 
-        elif command[0] in handler:
-            print(handler[command[0]](*command[1:]))
+        elif command in handler:
+            print(handler[command](record))
         else:
             print("Incorrect command. (To see all commands enter 'commands')")
 
